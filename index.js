bb83dbd8c2870afd81b448cca68ef94029d1e0aa
Jake Rayson <web@growdigital.org>
2017-03-15 22:30:01 +0000

    added metalsmith-concat plugin plus rejigged directory structure

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var Metalsmith  = require('metalsmith');
-var markdown    = require('metalsmith-markdown');
-var layouts     = require('metalsmith-layouts');
-var permalinks  = require('metalsmith-permalinks');
+var concat      = require('metalsmith-concat');
 var drafts      = require('metalsmith-drafts');
+var layouts     = require('metalsmith-layouts');
+var markdown    = require('metalsmith-markdown');
+var permalinks  = require('metalsmith-permalinks');
 
 Metalsmith(__dirname)
   .metadata({
@@ -11,12 +12,27 @@ Metalsmith(__dirname)
     generator: "Metalsmith",
     url: "https://growdigital.org/"
   })
+  // The order is important
+  .use(concat({
+    files:  [
+              'assets/css/settings/variables.css', 
+              'assets/css/settings/base.css', 
+              'assets/css/settings/responsive.css', 
+              'assets/css/objects/**/*.css', 
+              'assets/css/components/**/**/*.css', 
+              'assets/css/utilities/*.css', 
+              'assets/css/shame.css'
+            ],
+    output: 'assets/styles.css'
+  }))
   .source('./src')
   .destination('./dist')
-  .clean(false)
+  .clean(true)
   .use(drafts())
   .use(markdown())
-  .use(permalinks())
+  .use(permalinks({
+    pattern: './posts/:title'
+  }))
   .use(layouts({
     engine: 'handlebars'
   }))
--- src/posts/fourth-post.md
+++ src/posts/fourth-post.md
@@ -2,6 +2,7 @@
 title: My Fourth Post
 date: 2012-12-07
 layout: post.html
+draft: true
 ---
 
 A really short, rushed-feeling string of words.
